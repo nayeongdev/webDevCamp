634f58223fc36366ce4d6fc1dccdd78cb7335744
nayeongdev <nayeongkim.dev@gmail.com>
2023-08-08 22:44:48 +0900

    오버레이 표시&숨기기

---
+++ tictactoe-game/scripts/config.js
@@ -0,0 +1,10 @@
+// 모든 로직을 담당하는 파일
+function openPlayerConfig() {
+  palyerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+  palyerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+}
