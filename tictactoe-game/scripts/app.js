bd3ee17aec595d5f09e767f73625a1712cf3da48
nayeongdev <nayeongkim.dev@gmail.com>
2023-08-10 17:38:47 +0900

    순서에 맞는 게임 보드 선택 및 플레이어 표시

--- tictactoe-game/scripts/app.js
+++ tictactoe-game/scripts/app.js
@@ -1,14 +1,15 @@
 // 여러가지를 초기화시키는 가장 먼저 실행되어야 하는 파일
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
     name: '',
-    Symbol: 'X'
+    symbol: 'X'
   },
   {
     name: '',
-    Symbol: 'O'
+    symbol: 'O'
   },
 ];
 
@@ -17,11 +18,14 @@ const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const startNewGameBtnElement = document.getElementById('start-btn');
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -32,3 +36,8 @@ backdropElement.addEventListener('click', closePlayerConfig);
 formElement.addEventListener('submit', savePlayerConfig);
 
 startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener('click', selectGameField);
+// }
+gameBoardElement.addEventListener('click', selectGameField);
