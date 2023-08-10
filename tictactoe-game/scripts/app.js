d75075291ff3e76092d2817093c94222517a7f0f
nayeongdev <nayeongkim.dev@gmail.com>
2023-08-10 15:30:34 +0900

    사용자 입력 확인 - error 표시 추가

--- tictactoe-game/scripts/app.js
+++ tictactoe-game/scripts/app.js
@@ -1,6 +1,8 @@
 // 여러가지를 초기화시키는 가장 먼저 실행되어야 하는 파일
 const palyerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
--- tictactoe-game/scripts/config.js
+++ tictactoe-game/scripts/config.js
@@ -7,10 +7,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   palyerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
   const enteredPlayername = formData.get('playername').trim();  // '         ' => ''
+
+  if (!enteredPlayername) {  // enteredPlayername === ''
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
 }
