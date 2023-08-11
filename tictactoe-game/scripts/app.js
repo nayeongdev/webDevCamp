b7869292bd1d7e252d4770ef33138d7c74cf5dd7
nayeongdev <nayeongkim.dev@gmail.com>
2023-08-11 16:50:32 +0900

    게임 종료 및 재시작 로직 추가

--- tictactoe-game/scripts/app.js
+++ tictactoe-game/scripts/app.js
@@ -8,6 +8,7 @@ const gameData = [
 let editedPlayer = 0;
 let activePlayer = 0;
 let currentRound = 1;
+gameIsOver = false;
 
 const players = [
   {
@@ -26,6 +27,7 @@ const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
 const activePlayerNameElement = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
