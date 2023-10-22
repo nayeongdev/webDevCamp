fbf2d98b8a145a62b808af33abe5ced7e137d348
nayeongdev <nayeongkim.dev@gmail.com>
2023-10-23 01:14:44 +0900

    404에러 페이지 생성

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get('/restaurants/:id', function (req, res) {
       return res.render('restaurants-detail', { restaurant: restaurant });
     }
   }
+
+  res.render('404');
 });
 
 app.get('/recommend', function (req, res) {
