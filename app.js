8d8659533e862620ef0aaa54d0dc1d7b446179d7
nayeongdev <nayeongkim.dev@gmail.com>
2023-10-23 02:44:57 +0900

    사용자 정의 상태코드 지정

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/restaurants/:id', function (req, res) {
     }
   }
 
-  res.render('404');
+  res.status(404).render('404');
 });
 
 app.get('/recommend', function (req, res) {
@@ -69,11 +69,11 @@ app.get('/about', function (req, res) {
 });  // localhost:3000/about
 
 app.use(function (req, res) {
-  res.render('404');
+  res.status(404).render('404');
 })
 
 app.use(function (error, req, res, next) {
-  res.render('500');
+  res.status(500).render('500');
 })
 
 app.listen(3000);
